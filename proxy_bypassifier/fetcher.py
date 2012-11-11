e7e9ef04c98e1b14dc2b5244d04d9908743f8c8e
Enrique Ramirez <enriquein@gmail.com>
2012-11-10 22:15:48 -0400

    Validate url. More style tweaks. Url handling tweaks.

--- proxy_bypassifier/fetcher.py
+++ proxy_bypassifier/fetcher.py
@@ -1,7 +1,16 @@
 import urllib
 
 def get_url_contents(url):
-    f = urllib.urlopen()
-    s = f.read()
-    f.close()
-    return s
+    stripped = url.strip()
+
+    if (len(stripped) == 0 ):
+        raise Exception('URL cannot be blank.')
+        return
+
+    if not (url.strip().startswith( ('http://', 'https://') )):
+        stripped = 'http://' + stripped
+
+    tmp = urllib.urlopen(stripped)
+    contents = tmp.read()
+    tmp.close()
+    return contents
--- proxy_bypassifier/static/main.js
+++ proxy_bypassifier/static/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
     $('.button').button();
     $('.tabs').tabs();
+    $('#url').validate();
 });
